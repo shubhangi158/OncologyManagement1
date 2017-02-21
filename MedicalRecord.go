21a4aba85d9c4f3efe07133429b1cdd9489d387c
Shubhangi Badwaik <shubhangi_badwaik@persistent.co.in>
2017-02-21 14:56:55 +0530

    Modified: No Marshal & Unmarshal

--- MedicalRecord.go
+++ MedicalRecord.go
@@ -1,62 +1,27 @@
 package main
 
-import(
-	//"fmt"
+import (
 	"errors"
+	"fmt"
 	"github.com/hyperledger/fabric/core/chaincode/shim"
-	"encoding/json"
-	//"strconv"
 )
 
-var logger = shim.NewLogger("PatientRecord")
-
-type Chaincode struct{
+// SimpleChaincode example simple Chaincode implementation
+type SimpleChaincode struct {
 }
 
-type Contact struct{
-	ContactId string
-	Name string
-	Age int
-	Gender string
-	Race string
-}
-
-type Case struct{
-	N_Stage string
-	T_Stage string
-	Grade string
-	Condition string
-	Cancer_Diagnosis string
-	Cancer_Stage string
-	Metastasis_Location string
-	NCCN_Distress_Score	int
-	Survival_Time int									//Survival Time is in percentage.
-} 
-
-type BackgroundInformation struct{
-	Affected_Breast string
-	ER_Status_LB string
-	ER_Status_RB string
-	HER2_Status_LB string
-	HER2_Status_RB string
-	PR_Status_LB string
-	PR_Status_RB string
-}
-
-type MedicalRecord struct{
-	Contact_Rec Contact
-	Case_Rec Case
-	BackgroundInfo BackgroundInformation
-}
-
-func main(){
-	err := shim.Start(new(Chaincode))
-	if err != nil{
-		errors.New("MAIN: Error in starting chaincode.")
+func main() {
+	err := shim.Start(new(SimpleChaincode))
+	if err != nil {
+		fmt.Printf("Error starting Simple chaincode: %s", err)
 	}
 }
 
-func (c *Chaincode) Init(stub shim.ChaincodeStubInterface, function string, args []string) ([]byte, error){
+// Init resets all the things
+func (t *SimpleChaincode) Init(stub shim.ChaincodeStubInterface, function string, args []string) ([]byte, error) {
+	if len(args) != 1 {
+		return nil, errors.New("Incorrect number of arguments. Expecting 1")
+	}
 
 	err := stub.PutState(args[0], []byte(args[0]))
 	if err != nil {
@@ -66,112 +31,68 @@ func (c *Chaincode) Init(stub shim.ChaincodeStubInterface, function string, args
 	return nil, nil
 }
 
-func (c *Chaincode) Invoke(stub shim.ChaincodeStubInterface, function string, args []string) ([]byte, error){
-	
-	if len(args) != 21 {
-		errors.New("INVOKE: Incorrect number of arguments passed.")
+// Invoke isur entry point to invoke a chaincode function
+func (t *SimpleChaincode) Invoke(stub shim.ChaincodeStubInterface, function string, args []string) ([]byte, error) {
+	fmt.Println("invoke is running " + function)
+
+	// Handle different functions
+	if function == "init" {
+		return t.Init(stub, "init", args)
+	} else if function == "write" {
+		return t.write(stub, args)
 	}
-	
-	/*age, err := strconv.Atoi(args[2])
-	if err != nil{
-		errors.New("INVOKE: String to int conversion failed.")
-	}*/
-	
-	if function == "writeMedicalRecord" {
-		return c.writeMedicalRecord(stub, args)
-	} 
-	/*else if function == "updateMedicalRecord" {
-		return c.updateMedicalRecord(stub, args)
-	}*/
-	
+	fmt.Println("invoke did not find func: " + function)
+
+	return nil, errors.New("Received unknown function invocation: " + function)
+}
+
+// Query is our entry point for queries
+func (t *SimpleChaincode) Query(stub shim.ChaincodeStubInterface, function string, args []string) ([]byte, error) {
+	fmt.Println("query is running " + function)
+
+	// Handle different functions
+	if function == "read" { //read a variable
+		return t.read(stub, args)
+	}
+	fmt.Println("query did not find func: " + function)
+
+	return nil, errors.New("Received unknown function query: " + function)
+}
+
+// write - invoke function to write key/value pair
+func (t *SimpleChaincode) write(stub shim.ChaincodeStubInterface, args []string) ([]byte, error) {
+	var key, value string
+	var err error
+	fmt.Println("running write()")
+
+	if len(args) != 2 {
+		return nil, errors.New("Incorrect number of arguments. Expecting 2. name of the key and value to set")
+	}
+
+	key = args[0] //rename for funsies
+	value = args[1]
+	err = stub.PutState(key, []byte(value)) //write the variable into the chaincode state
+	if err != nil {
+		return nil, err
+	}
 	return nil, nil
 }
 
-func (c *Chaincode) Query(stub shim.ChaincodeStubInterface, function string, args []string) ([]byte, error){
+// read - query function to read key/value pair
+func (t *SimpleChaincode) read(stub shim.ChaincodeStubInterface, args []string) ([]byte, error) {
+	var key, jsonResp string
+	var err error
 
-	/*if len(args) != 1{
-		errors.New("QUERY: Incorrect number of arguments passed.")
-	}*/
-
-	if function == "readMedicalRecord" {
-		return c.readMedicalRecord(stub, args[0])
+	if len(args) != 1 {
+		return nil, errors.New("Incorrect number of arguments. Expecting name of the key to query")
 	}
 
-	return nil, nil
+	key = args[0]
+	valAsbytes, err := stub.GetState(key)
+	if err != nil {
+		jsonResp = "{\"Error\":\"Failed to get state for " + key + "\"}"
+		return nil, errors.New(jsonResp)
+	}
+
+	return valAsbytes, nil
 }
-
-/*func (c *Chaincode) writeMedicalRecord(stub shim.ChaincodeStubInterface, args []string) ([]byte, error){
-
-	if args[0] == ""{
-		return nil, errors.New("WRITE_MEDICAL_RECORD: Invalid Contact ID provided.")
-	}
-	
-	var cont Contact
-	contactJson  := []byte(`{"ContactId":"` + args[0] + `","Name":"` + args[1] + `","Age":` + args[2] + `,"Gender":"` + args[3] + `","Race":"` + args[4] + `"}`)
-	err := json.Unmarshal(contactJson, &cont)
-	if err != nil{
-		errors.New("WRITE_MEDICAL_RECORD: Invalid JSON object.")
-	}
-
-	var record MedicalRecord
-	recordJson  := []byte(`{"ContactId":"` + contactId + `","Name":"` + name + `","Age":` + strconv.Itoa(age) + `,"Gender":"` + gender + `","Race":"` + race + `"}`)
-	
-	err := json.Unmarshal(recordJson, &record)
-	if err != nil{
-		errors.New("WRITE_MEDICAL_RECORD: Invalid JSON object.")
-	}
-	
-	c.saveRecord(stub, cont)
-	
-	return nil, nil
-}*/
-
-func (c *Chaincode) writeMedicalRecord(stub shim.ChaincodeStubInterface, args []string) ([]byte, error){
-
-	if args[0] == ""{
-		return nil, errors.New("WRITE_MEDICAL_RECORD: Invalid Contact ID provided.")
-	}
-	
-	err := stub.PutState(args[0], []byte(args[1]))
-	if err != nil{
-		errors.New("SAVE_RECORD: Error saving Medical Record.")
-	}
-	
-	return nil, nil
-}
-
-func (c *Chaincode) saveRecord(stub shim.ChaincodeStubInterface, cont Contact) (bool, error){
-	
-	contact, err := json.Marshal(cont)
-	if err != nil{
-		errors.New("SAVE_RECORD: Error encoding Medical Record.")
-	}
-	
-	err = stub.PutState(cont.ContactId, contact)
-	if err != nil{
-		errors.New("SAVE_RECORD: Error saving Medical Record.")
-	}
-	
-	return true, nil
-}
-
-func (c *Chaincode) readMedicalRecord(stub shim.ChaincodeStubInterface, contactId string) ([]byte, error){
-
-	//var cont Contact
-
-	recordAsBytes, err := stub.GetState(contactId);
-	if err != nil{
-		errors.New("READ_MEDICAL_RECORD: Failed to get Medical Record")
-	}
-	
-	/*err = json.Unmarshal(recordAsBytes, &cont)
-	if err != nil{
-		errors.New("READ_MEDICAL_RECORD: Corrupt Medical Record")
-	}*/
-	
-	return recordAsBytes, nil
-}
-
-/*func (c *Chaincode) updateMedicalRecord(stub shim.ChaincodeStubInterface, args []string) ([]byte, error){
-	return nil, nil
-}*/
